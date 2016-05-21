7afc2adb91297db0809974fb4dca637ef72bf13a
0xFireball <0xFireball@outlook.com>
2016-05-20 23:29:21 -0700

    add avalon

--- Mystifier/Mystifier/MainWindow.xaml.cs
+++ Mystifier/Mystifier/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System.Windows;
+using System.Xml;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using ICSharpCode.AvalonEdit.Search;
 
 namespace Mystifier
 {
@@ -10,6 +15,23 @@ namespace Mystifier
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            LoadEditorTheme();
+        }
+
+        private void LoadEditorTheme()
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Mystifier.Resources.JavaScriptTheme.xshd"))
+            {
+                using (var reader = new XmlTextReader(stream))
+                {
+                    TextEditor.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                    SearchPanel.Install(TextEditor);
+                }
+            }
         }
     }
 }
