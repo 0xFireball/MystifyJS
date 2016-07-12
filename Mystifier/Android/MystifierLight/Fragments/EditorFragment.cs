f6204461a0cb75a88493115117267702cabab19b
0xFireball <0xFireball@outlook.com>
2016-07-12 11:00:59 -0700

    use relative path in obfuscation

--- Mystifier/Android/MystifierLight/Fragments/EditorFragment.cs
+++ Mystifier/Android/MystifierLight/Fragments/EditorFragment.cs
@@ -1,16 +1,11 @@
-using System;
-
 using Android.App;
 using Android.OS;
-using Android.Util;
 using Android.Views;
-using MystifierLightEditor.Controls;
 
 namespace MystifierLight.Fragments
 {
     public class EditorFragment : Fragment
     {
-
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
