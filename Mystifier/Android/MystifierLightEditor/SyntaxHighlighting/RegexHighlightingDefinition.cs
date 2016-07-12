2ecf6cafc0cd38399bab6f32c7d1b59cfdae0955
0xFireball <0xFireball@outlook.com>
2016-07-12 14:00:10 -0700

    do a full java api style refactory

--- Mystifier/Android/MystifierLightEditor/SyntaxHighlighting/RegexHighlightingDefinition.cs
+++ Mystifier/Android/MystifierLightEditor/SyntaxHighlighting/RegexHighlightingDefinition.cs
@@ -1,13 +1,20 @@
-using System.Text.RegularExpressions;
+using Android.Graphics;
+using Java.Util.Regex;
 
 namespace MystifierLightEditor.SyntaxHighlighting
 {
     public class RegexHighlightingDefinition
     {
-        public Regex LinePattern { get; set; } = new Regex(".*\\n");
-        public Regex Numbers { get; set; } = new Regex("\\b(\\d*[.]?\\d+)\\b");
-        public Regex Keywords { get; set; }
-        public Regex Builtins { get; set; }
-        public Regex Comments { get; set; }
+        public Pattern LinePattern { get; set; } = Pattern.Compile(".*\\n");
+        public Pattern NumbersPattern { get; set; } = Pattern.Compile("\\b(\\d*[.]?\\d+)\\b");
+        public Pattern KeywordsPattern { get; set; }
+        public Pattern BuiltinsPattern { get; set; }
+        public Pattern CommentsPattern { get; set; }
+
+        public Color ErrorColor { get; set; }
+        public Color NumberColor { get; set; }
+        public Color KeywordColor { get; set; }
+        public Color BuiltinColor { get; set; }
+        public Color CommentColor { get; set; }
     }
 }
